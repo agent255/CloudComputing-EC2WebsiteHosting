daadd1ef1b91e23bb822b00fee368f7de72ff1f2
agent355 <70315828+agent255@users.noreply.github.com>
2020-10-15 16:50:23 +0530

    Update credentials.js

--- weather-dashboard/credentials.js
+++ weather-dashboard/credentials.js
@@ -1,6 +1,6 @@
 var credentials = {
-  ipinfoAPIKEY: "YOUR API KEY HERE",
-  openWeatherMapAPIKEY: "YOUR API KEY HERE",
+  ipinfoAPIKEY: "7c8d85d7884d7e",
+  openWeatherMapAPIKEY: "005b8a9fd61386b63a60d2e7ec4a5f57",
 };
 
 //Get IPINFOAPI KEY at https://ipinfo.io/
